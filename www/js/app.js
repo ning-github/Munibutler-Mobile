61f83b6c94c1af7d746c27a51091b2c513a00510
Ning Xia <nxiamail@gmail.com>
2015-07-02 22:18:24 -0700

    (refactor) move custom directive into service file

--- www/js/app.js
+++ www/js/app.js
@@ -34,36 +34,6 @@ var muniButlerApp = angular.module('muniButler', [
       templateUrl: 'views/login.html',
       controller: ''
     });
-})
-// for d3 visualization
-.directive('changeColor', ['d3Service', function(d3Service) {
-  return {
-    restrict: 'E',
-
-    link: function(scope, element, attrs) {
-      d3Service.d3().then(function(d3) {
-        var green = "#00ff00";
-        var yellow = "#ffff00";
-        var red = "#ff0000";
-
-        //unique $index for each repeat
-
-        var colorScale = d3.scale.linear()
-          .domain([0, 20, 40])
-          .range([green, yellow, red]);
-
-        d3.selectAll('.duration')
-          .each(function(d, i){
-
-            var minutes = this.textContent.split(' ')[0];
-            console.log('SCOPE: ', scope);
-            d3.select(this)
-              .style('color', colorScale(minutes));
-          });
-          // .style('color', colorScale(minutes)); 
-        
-      });  
-    }}
-}]);
+});
 
 
